68a573c7a31b7e490570f7342e747090cf27bd64
samjjc <sam111jc@gmail.com>
2016-11-19 19:40:23 -0500

    emptied out activity main

--- app/src/main/java/com/example/johnny/wallhack/MainActivity.java
+++ app/src/main/java/com/example/johnny/wallhack/MainActivity.java
@@ -1,105 +1,15 @@
 package com.example.johnny.wallhack;
-
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int NUM_ROWS = 3;
-    private static final int NUM_COLS = 3;
-
-    Button buttons[][] = new Button [NUM_ROWS][NUM_COLS];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        populateButtons();
     }
 
-    private void populateButtons() {
-        TableLayout table = (TableLayout) findViewById(R.id.tableForButtons);
-        for (int row=0; row< NUM_ROWS;row++){
-            TableRow tableRow = new TableRow(this);
-            tableRow.setLayoutParams(new TableLayout.LayoutParams(
-                    TableLayout.LayoutParams.MATCH_PARENT,
-                    TableLayout.LayoutParams.MATCH_PARENT,
-                    1.0f
-            ));
-            table.addView(tableRow);
-
-            for (int col=0; col< NUM_COLS;col++){
-                final int FINAL_COL = col;
-                final int FINAL_ROW = row;
-                Button button = new Button(this);
-                button.setLayoutParams(new TableRow.LayoutParams(
-                        TableRow.LayoutParams.MATCH_PARENT,
-                        TableRow.LayoutParams.MATCH_PARENT,
-                        1.0f
-                ));
-                button.setText(""+col+","+row);
-                //make Text not clip on small buttons
-                button.setPadding(0,0,0,0);
-
-                button.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-                        gridButtonClicked(FINAL_COL, FINAL_ROW);
-                    }
-                });
-                tableRow.addView(button);
-                buttons[row][col] = button;
-            }
-        }
-    }
-
-    private void gridButtonClicked(int col, int row) {
-        Toast.makeText(this, "Button clicked" +col+ "," +row, Toast.LENGTH_SHORT).show();
-        Button button = buttons[row][col];
-        //Lock Button Sizes
-        LockButtonSizes();
-
-
-        //doesn't scale image
-        //button.setBackgroundResource(R.mipmap.raven_head);
-
-        //scale Image
-        int newWidth = button.getWidth();
-        int newHeight = button.getHeight();
-        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.raven_head);
-        Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth,newHeight,true);
-        Resources resource = getResources();
-        button.setBackground(new BitmapDrawable(resource, scaledBitmap));
-
-        //change text
-        button.setText("clicked");
-    }
-
-    private void LockButtonSizes() {
-        for(int r = 0; r<NUM_ROWS;r++){
-            for(int c = 0; c<NUM_COLS;c++){
-                Button button = buttons[c][r];
-
-                int width = button.getWidth();
-                button.setMinWidth(width);
-                button.setMaxWidth(width);
-
-                int height = button.getHeight();
-                button.setMinHeight(height);
-                button.setMaxHeight(height);
-
-            }
-        }
-    }
 
 
 }
